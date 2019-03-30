fc634a09116d37274194dbb4f64b0179b4ac4ba3
Stanislav Spirin <stanislaw.spirin@ya.ru>
2019-03-30 17:52:41 +0300

    добавил модалки

---
+++ js/main.js
@@ -0,0 +1,26 @@
+let modal = document.querySelector('#modal'),
+    closeBtn = document.querySelector('#close'),
+    btn = document.querySelectorAll('.modal-btn'),
+    modalBlock = document.querySelectorAll('.modal-block');
+
+function modalClose() {
+        modal.style.display = 'none';
+        document.body.style.overflow = 'auto';
+        for (i = 0; i <= modalBlock.length; i++) {
+            modalBlock[i].style.display = 'none';
+        }
+        
+    }
+closeBtn.addEventListener('click', function(){
+    modalClose();
+});
+
+for(i = 0; i < btn.length; i++) {
+    btn[i].addEventListener('click', function(e) {
+        modal.style.display = 'block';
+        document.body.style.overflow = 'hidden';
+        let name = e.target.name;
+        document.querySelector('#' + name).style.display = 'block';
+    })
+}
+
