a9c03bca2ee4b9f8103c14ae54f608d90d949e20
Stanislav Spirin <stanislaw.spirin@ya.ru>
2019-03-31 18:52:06 +0300

    мелкие правки

--- js/main.js
+++ js/main.js
@@ -4,23 +4,21 @@ let modal = document.querySelector('#modal'),
     modalBlock = document.querySelectorAll('.modal-block');
 
 function modalClose() {
-        modal.style.display = 'none';
-        document.body.style.overflow = 'auto';
-        for (i = 0; i < modalBlock.length; i++) {
-            modalBlock[i].style.display = 'none';
-        }
-        
+    modal.style.display = 'none';
+    document.body.style.overflow = 'auto';
+    for (i = 0; i < modalBlock.length; i++) {
+        modalBlock[i].style.display = 'none';
     }
-closeBtn.addEventListener('click', function(){
+}
+closeBtn.addEventListener('click', function () {
     modalClose();
 });
 
-for(i = 0; i < btn.length; i++) {
-    btn[i].addEventListener('click', function(e) {
+for (i = 0; i < btn.length; i++) {
+    btn[i].addEventListener('click', function (e) {
         modal.style.display = 'block';
         document.body.style.overflow = 'hidden';
         let name = e.target.name;
         document.querySelector('#' + name).style.display = 'block';
     })
 }
-
