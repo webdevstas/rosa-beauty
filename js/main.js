caf9d116a265ca31cd2b4ca968ca9f08154022f7
Stanislav Spirin <stanislaw.spirin@ya.ru>
2019-04-02 13:55:53 +0300

    proba

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 let modal = document.querySelector('#modal'),
+    shim = document.querySelector('#shim'),
     closeBtn = document.querySelector('#close'),
     btn = document.querySelectorAll('.modal-btn'),
     modalBlock = document.querySelectorAll('.modal-block');
 
 function modalClose() {
     modal.style.display = 'none';
+    shim.style.display = 'none';
     document.body.style.overflow = 'auto';
     for (i = 0; i < modalBlock.length; i++) {
         modalBlock[i].style.display = 'none';
@@ -17,6 +19,7 @@ closeBtn.addEventListener('click', function () {
 for (i = 0; i < btn.length; i++) {
     btn[i].addEventListener('click', function (e) {
         modal.style.display = 'block';
+        shim.style.display = 'block';
         document.body.style.overflow = 'hidden';
         let name = e.target.name;
         document.querySelector('#' + name).style.display = 'block';
