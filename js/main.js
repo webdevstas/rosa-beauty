f6c4120c9fe13ca417171f5a1bdc4995c15a78ce
Stanislav Spirin <stanislaw.spirin@ya.ru>
2019-04-02 17:52:40 +0300

    сделал кликабельными фотки спикеров

--- js/main.js
+++ js/main.js
@@ -2,7 +2,29 @@ let modal = document.querySelector('#modal'),
     shim = document.querySelector('#shim'),
     closeBtn = document.querySelector('#close'),
     btn = document.querySelectorAll('.modal-btn'),
-    modalBlock = document.querySelectorAll('.modal-block');
+    modalBlock = document.querySelectorAll('.modal-block'),
+    speakersImg = document.querySelectorAll('.speakers-img')
+
+
+closeBtn.addEventListener('click', function () {
+    modalClose();
+});
+shim.addEventListener('click', function() {
+    modalClose();
+})
+
+for (i = 0; i < btn.length; i++) {
+    btn[i].addEventListener('click', function (e) {
+        modalOpen(e);
+    })
+}
+
+for (i = 0; i < speakersImg.length; i++) {
+    speakersImg[i].addEventListener('click', function (e) {
+        modalOpen(e);
+    })
+}
+
 
 function modalClose() {
     modal.style.display = 'none';
@@ -13,20 +35,12 @@ function modalClose() {
         modalBlock[i].style.display = 'none';
     }
 }
-closeBtn.addEventListener('click', function () {
-    modalClose();
-});
-shim.addEventListener('click', function() {
-    modalClose();
-})
 
-for (i = 0; i < btn.length; i++) {
-    btn[i].addEventListener('click', function (e) {
-        modal.style.display = 'block';
-        shim.style.display = 'block';
-        closeBtn.style.display = 'block';
-        document.body.style.overflow = 'hidden';
-        let name = e.target.name;
-        document.querySelector('#' + name).style.display = 'block';
-    })
+function modalOpen(e) {
+    modal.style.display = 'block';
+    shim.style.display = 'block';
+    closeBtn.style.display = 'block';
+    document.body.style.overflow = 'hidden';
+    let name = e.target.name;
+    document.querySelector('#' + name).style.display = 'block';
 }
